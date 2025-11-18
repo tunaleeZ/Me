1bbbe3f99b9b7d76ce22d6c53cc74b593cd13c5f
tuan <tuanbnjkljkl456@gmail.com>
2025-11-18 14:34:20 +0700

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-python - << 'PY'
-import torch, torch_tensorrt, tensorrt as trt
-import torch_tensorrt.dynamo as ttrt
-print("Torch:", torch.__version__)
-print("Torch-TensorRT:", torch_tensorrt.__version__)
-print("TensorRT:", trt.__version__)
-print("CUDA OK:", torch.cuda.is_available())
-print("Dynamo compile OK:", hasattr(ttrt, "compile"))
-PY
